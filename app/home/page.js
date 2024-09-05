b236849de86df763e9bd1c2fa00dbcb450ab3fd5
RajatSharma <withrajatsharma@gmail.com>
2024-09-05 18:13:49 +0530

    home page

--- app/home/page.js
+++ app/home/page.js
@@ -1,42 +1,39 @@
-"use client";
+import React from 'react'
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
-import { Button } from "@/components/ui/button";
-import axios from "axios";
-import { useState } from "react";
 
-export default function Home() {
-
-  const [response,setResponse] = useState(null);
-
-  const getGeminiRes = async () => {
-    try {
-      const prompt = "hii";
-
-      const response = await axios.post(
-        `http://localhost:4000/user/geminires`,
-        {
-          prompt: prompt,
-        }
-      );
-
-      // console.log(response.data.message);
-      setResponse(response.data.message);
-    } catch (error) {
-      console.error("Error:", error.message);
-      throw error;
-    }
-  };
-
+const page = () => {
   return (
-    <div>
 
-        <Button onClick={getGeminiRes}>click to get response</Button>
-        {
-          response&&<h1>{response}</h1>
-        }
+    <main className='h-screen w-full pt-10 pl-32'>
 
+        <div className='bg-[#282796] p-8 w-[60%] h-[200px] rounded-2xl relative'>
+                    <div className='bg-gray-500 text-white flex justify-center items-center text-4xl rounded-full w-32 h-32 absolute right-0 translate-x-1/2 -translate-y-1/2 top-1/2'>
+                            R
+                    </div>
 
-      {/* <button >click to get response</button> */}
-    </div>
-  );
+                <div className=' text-white flex flex-col justify-between h-full'>
+                    <h1 className='text-2xl font-bold '>
+                        Adam Green
+                    </h1>
+
+                    <div>
+                        <p>Neurologist</p>
+                        <p className='text-sm  opacity-60 pt-1'>neurophysiologis MD PHD</p>
+                    </div>
+
+        <p className='opacity-80'>
+        2002 - beginning the medical career 
+        </p>
+
+                </div>
+                    
+
+        </div>
+
+    </main>
+
+    )
 }
+
+export default page
